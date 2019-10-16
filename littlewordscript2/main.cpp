4aa7d48a11f9c60c509b308c04cd7a375912225b
Drew Carlisle <drew.carlisle@yahoo.com>
2019-10-16 02:06:02 -0700

    Moved from std;:clock to chrono

--- littlewordscript2/main.cpp
+++ littlewordscript2/main.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <algorithm>
 #include <string>
+#include <chrono>
 #include "lwc_Parser.h"
 //Testing git push from different machine
 
@@ -15,7 +16,7 @@ void remove_whitespace(std::string &str) {
 
 int main(int argc, char* argv[])
 {
-	std::clock_t start = std::clock();
+	auto start = std::chrono::high_resolution_clock::now();
 	std::string file_name = (argc > 1 ? argv[1] : "first_test.lwc");
 	std::fstream fs;
 	std::string s;
@@ -36,15 +37,15 @@ int main(int argc, char* argv[])
 		lwc::block_func root_scope(lwc::parse_from_slines(words));
 		std::cout << "build complete" << std::endl;
 
-		std::clock_t start_eval = std::clock();
+		auto start_eval = std::chrono::high_resolution_clock::now();
 		lwc::evaluate_lines(root_scope);
-		std::clock_t end = std::clock();
-		int evaltime = (end - start_eval);
-		int fulltime = (end - start);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto evaltime = (end - start_eval);
+		auto fulltime = (end - start);
 		std::cout << "Done Evaluating" << std::endl << std::endl;
-		std::cout << "FULL RUNNING TIME: " << fulltime << std::endl;
-		std::cout << "BUILDING TIME: " << fulltime - evaltime << std::endl;
-		std::cout << "BUILT EVALUATION TIME: " << evaltime << std::endl;
+		std::cout << "FULL RUNNING TIME: " << std::chrono::duration<double, std::milli>(fulltime).count() << std::endl;
+		std::cout << "BUILDING TIME: " << std::chrono::duration<double, std::milli>(fulltime - evaltime).count() << std::endl;
+		std::cout << "BUILT EVALUATION TIME: " << std::chrono::duration<double, std::milli>(evaltime).count() << std::endl;
 
 		std::cout << std::endl;
 	}
